f9166e4288ccb2a06ebf3114e0865d58576b4334
Muhammad Zuhdiyazmi <mzuhdiyazmi@gmail.com>
2022-09-08 10:21:20 +0700

    update pw

--- database/conn.js
+++ database/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "password",
   database: "db_kontak",
 });
 
